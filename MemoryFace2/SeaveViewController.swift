77009db9ad75e37e427cae8c26d3bb77ddd2f090
291Airi <loveairi291@gmail.com>
2020-11-28 20:34:51 +0900

    urlの取得に困っています。 よろしくお願いします！

--- MemoryFace2/SeaveViewController.swift
+++ MemoryFace2/SeaveViewController.swift
@@ -18,16 +18,12 @@ class personArray: Object{
        @objc dynamic var pictureurl: String = ""
    }
 
-
-
-
-
 class SeaveViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate,UITextFieldDelegate{
     
     var textFieldString = ""
     var hint1 = ""
     var hint2 = ""
-    var  URL = ""
+    var URL = ""
     private var realm: Realm!
     
     @IBOutlet weak var nameTextField: UITextField!
@@ -52,63 +48,39 @@ class SeaveViewController: UIViewController, UIImagePickerControllerDelegate, UI
         imagePickerCountroller.allowsEditing = true
         //フォットライブラリを呼び出す
         self.present(imagePickerCountroller, animated: true, completion: nil)
-    }
-    
         //アルバム画面で写真を選択した時
-           func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
-            
-            //        //string型に変換、保存
-            //        let Imageinfo = info[UIImagePickerController.InfoKey.originalImage] as! UIImage
-            //
-            //        //imageに選んだ画像を設定する
-            //        let image = info[.originalImage] as? UIImage
-            //        let imageUrl = info[UIImagePickerController.InfoKey.referenceURL] as? NSURL
-            //        URL = (imageUrl?.absoluteString)!
-            //        //imageをpictureImageViewに設定する
-            //        pictureImageView.image = image
-                    //フォトライブラリを閉じる
-            
+        func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
             // defer はブロックを抜ける時に defer 内の処理が実行されます
-               defer {
-                  picker.dismiss(animated: true, completion: nil)
-               }
-               // 選択した画像から PHAsset を取得
-               // 画像ライブラリへのアクセスが許可されていない場合は nil が返ってきます
-               guard let phAsset = info[.phAsset] as? PHAsset else { return }
-            
-               let options = PHContentEditingInputRequestOptions()
-               phAsset.requestContentEditingInput(with: options) { (input, info) in
-                   // fullSizeImageURL に選択した画像のURLが入っているのでアンラップします
-                   guard let URL = input?.fullSizeImageURL else { return }
+            defer {
+               picker.dismiss(animated: true, completion: nil)
+            }
 
-                   // これを Realm に保存するなりしてください
-                   // Realm だと URL は保存できないと思うので、Stringに変換するといいかもしれないです
-                
-                let obj = Obj()
-                let url =  try! String()
-                obj.pictureurl = url
-                     
-                let realm = try! Realm()
-                
-                try! realm.writer{
-                    realm.add(obj)
-                }
-                
-                //let imageUrl = /*Realmから読み込んだURLのStringだと思ってください*/
-                let pictureurl = URL(string: urlString) {
-                    do {
-                        let data = try Data(contentsOf: url)
+            // 選択した画像から PHAsset を取得
+            //画像ライブラリへのアクセスが許可されていない場合は nil が返ってきます
+            guard let phAsset = info[.phAsset] as? PHAsset else { return }
 
-                        guard let pictureImageView = UIImage(data: data) else { return }
-                        pictureImageView.image = image
-                    } catch {
-                        print(error)
-                    }
-                }
-                   print(url.absoluteString)
-               }
-        
-        self.dismiss(animated: true, completion: nil)
+            let options = PHContentEditingInputRequestOptions()
+            phAsset.requestContentEditingInput(with: options) { (input, info) in
+                // fullSizeImageURL に選択した画像のURLが入っているのでアンラップします
+                guard let url = input?.fullSizeImageURL else { return }
+                
+                
+                print(url.absoluteString)
+               
+            }
+            let urlstring = URL
+            let url = String(urlstring)
+            do{
+                //URLからファイルの中身を所得する
+                let data = try Data(contentsOf:     url)
+                
+                guard let image = UIImage(data: data) else { return }
+                pictureImageView.image = image
+            } catch {
+                print(error)
+            }
+
+        }
     }
     
     
@@ -128,7 +100,6 @@ class SeaveViewController: UIViewController, UIImagePickerControllerDelegate, UI
         //personを格納するリストの作成
         let persons = List<personArray>()
         persons.append(person1)
-        //print("person1",persons)
         
         // Realmのインスタンスを生成する
         let realm = try! Realm()
@@ -136,8 +107,6 @@ class SeaveViewController: UIViewController, UIImagePickerControllerDelegate, UI
         // 書き込みトランザクション内でデータを追加する
         try! realm.write {
             realm.add(persons)
-        }
-        
         }
     
          func textFieldShouldReturn(_ textField: UITextField) -> Bool {
@@ -148,4 +117,4 @@ class SeaveViewController: UIViewController, UIImagePickerControllerDelegate, UI
     
     }
 
-
+}
